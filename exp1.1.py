30faf2500afcfcf89929fbd49059bce513474b98
chirag jindal <golujindal76@gmail.com>
2023-02-13 18:40:39 +0530

    experiment 1.1

--- exp1.1.py
+++ exp1.1.py
@@ -1,4 +1,7 @@
+#arthemetic operations
+print("enter the first value")
 a=float(input())
+print("enter the second value")
 b=float(input())
 
 c=a+b
@@ -7,4 +10,15 @@ e=a*b
 f=a/b
 g=a%b
 
-print(c,d,e,f,g)
+print('addition =',c,'\nsubstraction =',d,'\nmultiplication =',e,'\ndivision',f,'\nreminder',g)
+
+
+# average and percentage of five subject
+a=0
+print("Enter the marks of five subjects")
+for i in range (5):
+    a+=int(input())
+print('Total marks=',a,'\naverage marks =',a/5,'\npercentage =',a/5,"%")
+
+
+#convertion
