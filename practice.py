d85e2cada146f9b4381737762541e2bf3e250c7e
chirag jindal <golujindal76@gmail.com>
2023-02-27 18:19:52 +0530

    experiment 1.3

--- practice.py
+++ practice.py
@@ -1,15 +1,15 @@
-# def area(r):
-#     pie=22/7
-#     ar=pie*r*r
-#     print("area = %.2f"%ar)
-#     return ar
-# i=0
-# for i in range(10):
-#     a=int(input("enter radius:"))
-#     area(a)
+def area(r):
+    pie=22/7
+    ar=pie*r*r
+    print("area = %.2f"%ar)
+    return ar
+i=0
+for i in range(2):
+    a=int(input("enter radius:"))
+    area(a)
 
 
-#
+
 lst = []
 n = int(input("Enter number of elements : "))
 for i in range(0, n):
