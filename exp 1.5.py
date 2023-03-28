8554ad95a80c067df302faa4750e8a8f34d866a1
chirag jindal <golujindal76@gmail.com>
2023-03-28 19:10:07 +0530

    .

--- exp 1.5.py
+++ exp 1.5.py
@@ -1,16 +1,22 @@
-def last(n):
-     return n[-1]
-
-
-def sort(a):
-     return sorted(a, key=last)
-
-
-a = [(1, 2, 3), (3, 3, 4), (2, 1, 8)]
-print("Sorted:")
-print(sort(a))
+# def last(n):
+#      return n[-1]
+#
+#
+# def sort(a):
+#      return sorted(a, key=last)
+#
+#
+# a = [(1, 2, 3), (3, 3, 4), (2, 1, 8)]
+# print("Sorted:")
+# print(sort(a))
 
 # from operator import itemgetter as i
 # a = [(1, 2, 3), (3, 3, 4), (2, 1, 8)]
 # a.sort(key=i(2))
 # print (a)
+
+
+a = ['a', 'b', 'c', 'd', 'e', 'f']
+del a[5], a[4], a[0]
+print (a)
+
