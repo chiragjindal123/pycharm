5c081eb87ae9bf634565ba81baf12bcc0070b182
chirag jindal <golujindal76@gmail.com>
2023-04-25 11:15:27 +0530

    .

--- exp 1.5.py
+++ exp 1.5.py
@@ -10,13 +10,16 @@
 # print("Sorted:")
 # print(sort(a))
 
-from operator import itemgetter as i
-a = [(1, 2, 3), (3, 3, 4), (2, 1, 8)]
-a.sort(key=i(2))
-print (a)
+# from operator import itemgetter as i
+# a = [(1, 2, 3), (3, 3, 4), (2, 1, 8)]
+# a.sort(key=i(2))
+# print (a)
 
 #
 # a = ['a', 'b', 'c', 'd', 'e', 'f']
 # del a[5], a[4], a[0]
 # print (a)
 
+x=dict()
+x['name']='chirag'
+print(x['name'])
---
+++ exp 6.py
@@ -0,0 +1,18 @@
+a = {1:'ram',2:'rahul',3:'prince'}
+print(a)
+print(len(a))
+
+x=a.keys()
+print(x)
+
+y=a.get(2)
+print(y)
+
+a.update({4:'chirag'})
+print(a)
+
+c=a.copy()
+print(c)
+
+q=a.clear()
+print(q)
